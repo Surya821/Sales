9769169f64a30fd6e98cd2f9086f2f7370d3893f
Surya Pratap Singh <surya30082005@gmail.com>
2025-11-11 12:12:53 +0530

    minor fixes

--- app.py
+++ app.py
@@ -8,16 +8,18 @@ app = Flask(__name__)
 with open('happy.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Define a function to preprocess the input data
+# Preprocess input
 def preprocess_input(input_str):
     input_list = input_str.split(',')
     input_array = np.array(input_list, dtype=np.float32)
     return input_array.reshape(1, -1)
 
+# Home route
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Predict route
 @app.route('/predict', methods=['POST'])
 def predict():
     input_str = request.form['input_string']
@@ -26,5 +28,6 @@ def predict():
     output_message = f'The predicted sales value is {prediction:.2f}'
     return render_template('index.html', prediction_text=output_message)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1200,debug=True)
+# Only keep the app object; do not run server on Vercel
+# if __name__ == '__main__':
+#     app.run(debug=True)
